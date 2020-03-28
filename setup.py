68c3a8f2139ce33ded15bfa82fa876c690816217
Omran Jamal <o.jamal97@gmail.com>
2020-03-28 14:37:38 +0600

    Working PyPI Documentation

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='autocontainer',
     packages=['src'],
-    version='1.0.1',
+    version='1.0.2',
     license='MIT',
     description='A modern typing based service container and dependency injector.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Omran Jamal',
     author_email='o.jamal97@gmail.com', 
     url='https://github.com/Hedronium/autocontainer',
-    download_url='https://github.com/Hedronium/autocontainer/archive/v1.0.1.tar.gz',
+    download_url='https://github.com/Hedronium/autocontainer/archive/v1.0.2.tar.gz',
     keywords=['container', 'dependency', 'injection', 'inversion', 'control', 'service'],
     python_requires='>=3.5',
     classifiers=[
