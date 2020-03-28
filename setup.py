6f0bcc212532eda7bde1ee77d8bb88d36c9b436e
Omran Jamal <o.jamal97@gmail.com>
2020-03-28 13:47:36 +0600

    Version bump

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='autocontainer',
     packages=['src'],
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     description='A modern typing based service container and dependency injector.',
     author='Omran Jamal',
     author_email='o.jamal97@gmail.com', 
     url='https://github.com/Hedronium/autocontainer',
-    download_url='https://github.com/Hedronium/autocontainer/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/Hedronium/autocontainer/archive/v1.0.1.tar.gz',
     keywords=['container', 'dependency', 'injection', 'inversion', 'control', 'service'],
     python_requires='>=3.5',
     classifiers=[
